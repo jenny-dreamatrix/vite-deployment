cbab4c00b9f08e5e541174d33342e531531083fe
„jenny-dreamatrix“ <jenny.dreamatrix@gmail.com>
2024-06-24 11:44:57 +0200

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/vite-deploy/',
+  base: '/vite-deployment/',
   build: {
     rollupOptions: {
       input: {
